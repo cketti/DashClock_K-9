ea866894fccece7c85f42c4e1e7c5ae6b41fd85b
cketti <cketti@gmail.com>
2013-03-27 15:43:01 +0100

    Add code to display the unread count for all K-9 Mail accounts

--- src/de/cketti/dashclock/k9/K9Extension.java
+++ src/de/cketti/dashclock/k9/K9Extension.java
@@ -1,19 +1,34 @@
 package de.cketti.dashclock.k9;
 
+import java.util.List;
+
 import com.google.android.apps.dashclock.api.DashClockExtension;
 import com.google.android.apps.dashclock.api.ExtensionData;
+
+import de.cketti.dashclock.k9.K9Helper.Account;
 
 
 public class K9Extension extends DashClockExtension {
 
     @Override
     protected void onUpdateData(int reason) {
+
+        int unreadCount = 0;
+
+        List<Account> accounts = K9Helper.getAccounts(this);
+        if (accounts != null) {
+            for (Account account : accounts) {
+                int accountUnread = K9Helper.getUnreadCount(this, account);
+                unreadCount += accountUnread;
+            }
+        }
+
         ExtensionData data = new ExtensionData()
-                .visible(true)
+                .visible(unreadCount > 0)
                 .icon(R.drawable.ic_envelope)
-                .status("42")
-                .expandedTitle("Title here")
-                .expandedBody("More text here");
+                .status(Integer.toString(unreadCount))
+                .expandedTitle(getString(R.string.unread_title, unreadCount))
+                .expandedBody("K-9 Mail");
 
         publishUpdate(data);
     }
