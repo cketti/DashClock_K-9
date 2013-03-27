4044564b2ac6c093e74a40b32797ecc9346edddc
cketti <cketti@gmail.com>
2013-03-27 15:47:12 +0100

    Display number of unread messages per account

--- src/de/cketti/dashclock/k9/K9Extension.java
+++ src/de/cketti/dashclock/k9/K9Extension.java
@@ -14,12 +14,21 @@ public class K9Extension extends DashClockExtension {
     protected void onUpdateData(int reason) {
 
         int unreadCount = 0;
+        StringBuilder body = new StringBuilder();
 
         List<Account> accounts = K9Helper.getAccounts(this);
         if (accounts != null) {
             for (Account account : accounts) {
                 int accountUnread = K9Helper.getUnreadCount(this, account);
                 unreadCount += accountUnread;
+
+                if (accountUnread > 0) {
+                    if (body.length() > 0) {
+                        body.append('\n');
+                    }
+
+                    body.append(account.name).append(" (").append(accountUnread).append(')');
+                }
             }
         }
 
@@ -28,7 +37,7 @@ public class K9Extension extends DashClockExtension {
                 .icon(R.drawable.ic_envelope)
                 .status(Integer.toString(unreadCount))
                 .expandedTitle(getString(R.string.unread_title, unreadCount))
-                .expandedBody("K-9 Mail");
+                .expandedBody(body.toString());
 
         publishUpdate(data);
     }
