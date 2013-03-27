ea2c99ac3997c3889d320fb7adfe1082e8879396
cketti <cketti@gmail.com>
2013-03-27 15:26:10 +0100

    Display static text in DashClock

--- src/de/cketti/dashclock/k9/K9Extension.java
+++ src/de/cketti/dashclock/k9/K9Extension.java
@@ -1,14 +1,20 @@
 package de.cketti.dashclock.k9;
 
 import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
 
 
 public class K9Extension extends DashClockExtension {
 
     @Override
     protected void onUpdateData(int reason) {
-        // TODO Auto-generated method stub
+        ExtensionData data = new ExtensionData()
+                .visible(true)
+                .status("42")
+                .expandedTitle("Title here")
+                .expandedBody("More text here");
 
+        publishUpdate(data);
     }
 
 }
