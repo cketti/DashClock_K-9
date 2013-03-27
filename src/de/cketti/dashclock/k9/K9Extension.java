3c8a629531662ada7a479a99271170eeaaf081cf
cketti <cketti@gmail.com>
2013-03-27 15:22:35 +0100

    Add empty DashClockExtension
    This is enough to show up in DashClock's list of extensions.

---
+++ src/de/cketti/dashclock/k9/K9Extension.java
@@ -0,0 +1,14 @@
+package de.cketti.dashclock.k9;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+
+
+public class K9Extension extends DashClockExtension {
+
+    @Override
+    protected void onUpdateData(int reason) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
